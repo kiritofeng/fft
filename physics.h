2c2defabc9c41209ab6b55c6553bc33e75714c24
kiritofeng <rogerjfu@hotmail.com>
2018-08-29 10:37:51 -0400

    Create physics.h

---
+++ physics.h
@@ -0,0 +1,16 @@
+/**
+    physics.h
+    Purpose: includes all headers, allowing for simplier imports
+
+    @author Kirito Feng
+    @version 1.0
+*/
+
+#ifndef PHYSICS_H
+
+#define PHYSICS_H
+
+#include "fft.h"
+#include "matrix.h"
+
+#endif
