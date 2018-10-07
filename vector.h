36aa0a6de5ddb628472ac659a3158f4a48b00443
kiritofeng <rogerjfu@hotmail.com>
2018-10-07 14:00:59 -0400

    Add vector copy constructor

--- vector.h
+++ vector.h
@@ -7,35 +7,37 @@ class vector {
     public:
         T x, y, z;
 
-    vector(T _x, T _y, T _z = T(0)): x(_x), y(_y), z(_z){}
+        vector<T>(const vector<T> &v): x(v.x), y(v.y), z(v.z) {}
 
-    inline vector<T> operator + (const vector<T> &v) const {
-        return vector<T>(x + v.x, y + v.y, z + v.z);
-    }
+        vector(T _x, T _y, T _z = T(0)): x(_x), y(_y), z(_z){}
 
-    inline vector<T> operator - () const {
-        return vector<T>(-x, -y, -z);
-    }
+        inline vector<T> operator + (const vector<T> &v) const {
+            return vector<T>(x + v.x, y + v.y, z + v.z);
+        }
 
-    inline vector<T> operator - (const vector<T> &v) {
-        return (*this) + (-v);
-    }
+        inline vector<T> operator - () const {
+            return vector<T>(-x, -y, -z);
+        }
 
-    inline vector<T> operator * (const T &t) const {
-        return vector<T>(t * x, t * y, t * z);
-    }
+        inline vector<T> operator - (const vector<T> &v) {
+            return (*this) + (-v);
+        }
 
-    inline T operator * (const vector<T> &v) const {
-        return x * v.x + y * v.y + z * v.z;
-    }
+        inline vector<T> operator * (const T &t) const {
+            return vector<T>(t * x, t * y, t * z);
+        }
 
-    inline vector<T> operator ^ (const vector<T> &v) const {
-        return vector<T>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
-    }
+        inline T operator * (const vector<T> &v) const {
+            return x * v.x + y * v.y + z * v.z;
+        }
 
-    inline T magnitude() const {
-        return (*this) * (*this);
-    }
+        inline vector<T> operator ^ (const vector<T> &v) const {
+            return vector<T>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+        }
+
+        inline T magnitude() const {
+            return (*this) * (*this);
+        }
 };
 
 #endif
